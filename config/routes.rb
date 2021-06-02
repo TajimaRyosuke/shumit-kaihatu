294ae4e3cf823fa3fa7d7e9c69e7fd7fce0ee6fb
TajimaRyosuke <east.helman56@gmail.com>
2021-06-02 13:28:52 +0000

    [update]devise loginlogout機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,22 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_for :users
   root :to => 'homes#top'
 
   resources :users
-  post 'follow/:id' => 'relationships#follow', as: 'follow' #フォローする
-  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' #フォローを外す
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォローを外す
   get '/users/:id/following(.:format)' => 'users#following', as: 'following_user'
   get '/users/:id/followers(.:format)' => 'users#followers', as: 'followers_user'
 
   resources :posts do
-    resource :favorites, only: [:create, :destroy] #いいね
-    resources :post_comments, only: [:create] #コメント投稿
+    resource :favorites, only: [:create, :destroy] # いいね
+    resources :post_comments, only: [:create] # コメント投稿
   end
 
-  get 'search' => 'searches#search' #検索機能
+  get 'search' => 'searches#search' # 検索機能
 
-  get 'chat/id' => 'chats#show', as: 'chat' #DMチャット
+  get 'chat/id' => 'chats#show', as: 'chat' # DMチャット
   resources :chats, only: [:create]
 
 
