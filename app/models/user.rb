5ca0a5dfd40fba65dd402c987b6aa5a59a61aa88
TajimaRyosuke <east.helman56@gmail.com>
2021-06-04 08:19:55 +0000

    [update]usercontroller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attachment :profile_image #Refileで画像を反映させるときにviewに対応する各modelに記述が必須
+  has_many :posts, dependent: :destroy
+
+  has_many :post_comments, dependent: :destroy
+
+  has_many :favorites, dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key:"follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key:"followed_id", dependent: :destroy
+
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, though: :followed, source: :follower
+
+  def follow(user_id)
+    follower.create(followed_id: user_i)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, though: :passive_relationships, source: :follower
+
+  has_many :user_rooms
+  has_many :chats
+
 end
