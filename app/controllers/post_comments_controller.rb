425926b7747a81990998515401172bce2f72bc2b
TajimaRyosuke <east.helman56@gmail.com>
2021-06-04 14:15:01 +0000

    [update]コメント登録

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,4 +1,13 @@
 class PostCommentsController < ApplicationController
     def create
+        @post = Post.find(params[:post_id])
+        @post_comment = current_user.post_comments.new(post_comment_params)
+        @post_comment.post_id = @post.id
+        @post_comment.save
+    end
+
+    private
+    def post_comment_params
+        params.require(:post_comment).permit(:comment, :post_id, :user_id)
     end
 end
