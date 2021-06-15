e99f964ad2894258f5876811124515e7fda78f95
TajimaRyosuke <east.helman56@gmail.com>
2021-06-15 11:24:12 +0000

    [update]cssレイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.all
+    @posts = @user.posts.all.order(created_at: :desc)
   end
 
   def edit
