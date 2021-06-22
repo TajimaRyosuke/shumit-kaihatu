535e5c5e2c1e265c494e10fa08bcb93438f54912
TajimaRyosuke <east.helman56@gmail.com>
2021-06-22 08:51:22 +0000

    [update]Rspec追加

---
+++ spec/factories/post.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    post_title { Faker::Lorem.characters(number:10) }
+    opinion { Faker::Lorem.characters(number:50) }
+  end
+end
---
+++ spec/system/posts_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe '投稿のテスト' do
+  let!(:post) { create(:post, post_title:'hoge',opinion:'body') }
+  describe 'トップ画面(root_path)のテスト' do
+    before do
+      visit root_path
+    end
+    context '表示の確認' do
+      it 'root_pathが"/"であるか' do
+        expect(current_path).to eq('/homes')
+      end
+    end
+  end
+end
+
+describe "ログイン後のトップページの確認" do
+  before do
+    visit posts_path
+  end
+  context '表示の確認' do
+    it 'posts_pathが"/posts_path"であるか' do
+      expect(current_path).to eq('/posts')
+    end
+  end
+end
+
+  describe "投稿画面(new_post_path)のテスト" do
+    before do
+      visit new_post_path
+    end
+    context '表示の確認' do
+      it 'new_post_pathが"/posts/new"であるか' do
+        expect(current_path).to eq('/posts/new')
+      end
+    end
+  end
