0a310b94e46cffae3ace049368a8b932c02158ac
TajimaRyosuke <east.helman56@gmail.com>
2021-06-25 10:56:49 +0000

    [update]admin

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,5 +1,12 @@
 class Admin::PostsController < ApplicationController
   def index
+    # 全権一覧表示
+    @posts = Post.all.order(created_at: :desc)
+    # ジャンル別一覧表示
+    @genre = Genre.find(params[:id]) if params[:id]
+    @genres = Genre.all.order(created_at: :desc)
+    # ユーザー一覧表示
+    @users = User.all.order(created_at: :desc)
   end
 
   def show
